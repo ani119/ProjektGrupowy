753953c7526f1bae14001d8570e4831044d43895
przemytka <61848761+przemytka@users.noreply.github.com>
2020-04-16 16:40:10 +0200

    Merge branch '#20-add-name-scene' into add-base-scenes

---
+++ podwodny_swiat_tabliczki_mnozenia/Assets/Scripts/ShoalController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoalController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject fishPrefab;
+    public int fishAmount=1;
+    public float fishGap=1;
+    void generateShoal()
+    {
+        Quaternion q = new Quaternion();
+        q.Set(0.0f, 1.0f, 0.0f, 0.0f);
+        for (int i=0;i<fishAmount;i++)
+        {
+            GameObject textObject = (GameObject)Instantiate(fishPrefab, 
+                new Vector3(0, i*fishGap, 0), q);
+        }
+    }
+    void Start()
+    {
+        generateShoal();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
