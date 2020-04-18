955161a138ddad9988b01bacf24281ffc5ddb54e
juliakobusinska <44616508+juliakobusinska@users.noreply.github.com>
2020-04-18 13:00:33 +0200

    #35 create draggable tiles
    create 10 tiles for shoals
    create clones of tiles on drag

---
+++ podwodny_swiat_tabliczki_mnozenia/Assets/Scripts/ShoalDrag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoalDrag : MonoBehaviour
+{
+
+
+    public GameObject prefab;
+    public Vector3 newposition;
+    int index = 0;
+    public GameObject obj;
+
+    Vector3 mousePos;
+    public void BeginDrag()
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = 2.0f;
+        newposition = Camera.main.ScreenToWorldPoint(mousePos);
+        obj = Instantiate(prefab, newposition, Quaternion.identity);
+        obj.transform.SetParent(null);
+        obj.name = this.name +"_"+ index.ToString();
+        index++;
+    }
+    public void OnMouseDrag()
+
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = 2.0f;
+        newposition = Camera.main.ScreenToWorldPoint(mousePos);
+        obj.transform.position = newposition;
+
+    }
+}
