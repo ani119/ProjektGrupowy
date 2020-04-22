80385e245b7975a6e843f17662e38fa2cfc3a29d
AgaMazur <agnieszka.mazur15@gmail.com>
2020-04-22 22:45:08 +0200

    #29 Connect Menu
    - connect menu scenes
    
    - on click 'graj' go to 'podaj imię' scene
    - on click 'dalej' go to 'menu' scene
    - on click 'rybki' go to 'fishgame' scene

---
+++ podwodny_swiat_tabliczki_mnozenia/Assets/Scritps/Handlers/MenuButtonHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonHandler : MonoBehaviour
+{
+    public void FishClick()
+    {
+        SceneManager.LoadScene("FishGame");
+        Debug.Log("Rybki, rybeczki, rybunie...");
+    }
+
+    public void NumbersClick()
+    {
+        Debug.Log("Cyferki.");
+    }
+}
