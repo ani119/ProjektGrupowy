f7544cc15f6e1bb1854a112b089d82be9e941e5d
Wiki13002 <wiktoria.szultka97@gmail.com>
2020-04-23 17:20:17 +0200

    #34 Add equation
    -equation display
    -enter the result of the equation
    -verification of the enter equation result

---
+++ podwodny_swiat_tabliczki_mnozenia/Assets/Scritps/Multiplication.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Multiplication : MonoBehaviour
+{
+    private int multiplicand ;
+    private int multiplier;
+    private string equation;
+    public Text showEquation;
+    public InputField answer;
+    public Text error;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        error.text = "";
+        RandomEquation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            
+            CheckAnswer();
+        }
+    }
+    public void RandomEquation()
+    {
+        multiplicand = Random.Range(1, 11);
+        multiplier = Random.Range(1, 11);
+        equation = multiplicand.ToString() + " * " + multiplier.ToString() + "=";
+        showEquation.text = equation;
+    }
+    public void CheckAnswer()
+    {
+        if (answer.text == (multiplicand * multiplier).ToString())
+        {
+            error.text = "Bardzo dobrze! Następne równanie.";
+            RandomEquation();
+
+        }
+        else
+        {
+            error.text = "Zła odpowiedź. Spróbuj jeszcze raz!";
+        }
+    }
+}
