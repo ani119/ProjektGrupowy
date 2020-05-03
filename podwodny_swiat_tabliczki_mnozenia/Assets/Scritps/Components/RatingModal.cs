05918fa15764168689313f15b26db85f6199e235
AgaMazur <agnieszka.mazur15@gmail.com>
2020-05-03 22:11:25 +0200

    #44 Modal every 10 equations
    - add rating modal
    - add equation counter
    - add mistakes counter

---
+++ podwodny_swiat_tabliczki_mnozenia/Assets/Scritps/Components/RatingModal.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RatingModal : MonoBehaviour
+{
+    public Text text;
+    public Button closeButton;
+
+    private int total = 0;
+    private int tries = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        HideModal();
+
+        Button btn = closeButton.GetComponent<Button>();
+        btn.onClick.AddListener(HideModal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private int CalculateAccuracy()
+    {
+        double acc = (double)total / tries;
+        return (int)(acc * 100);
+    }
+
+    public void ShowRating(int tries, int total)
+    {
+        this.tries = tries;
+        this.total = total;
+
+        string s = CalculateAccuracy() + "%";
+        text.text = s;
+
+        this.gameObject.SetActive(true);
+    }
+
+    public void HideModal()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
