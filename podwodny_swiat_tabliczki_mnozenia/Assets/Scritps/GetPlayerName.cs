246f9a8456904480b36bfbcf35c0012b83a6e694
ani119 <karolina_makuch_email@wp.pl>
2020-05-04 16:25:07 +0200

    Remember player name
    - Add Globals script

--- podwodny_swiat_tabliczki_mnozenia/Assets/Scritps/GetPlayerName.cs
+++ podwodny_swiat_tabliczki_mnozenia/Assets/Scritps/GetPlayerName.cs
@@ -6,13 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class GetPlayerName : MonoBehaviour
 {
-    private string playerName;
     public GameObject inputField; 
 
     public void Button_Click()
     {
-        playerName = inputField.GetComponent<Text>().text;
-        Debug.Log( "Witaj " + playerName + " w podwodnym świecie tabliczki mnożenia!");
+        Globals.playerName = inputField.GetComponent<Text>().text;
+        Debug.Log( "Witaj " + Globals.playerName + " w podwodnym świecie tabliczki mnożenia!");
         SceneManager.LoadScene("Menu");
     }
 }
