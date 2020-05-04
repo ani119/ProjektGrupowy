b8c06e9a5f351d789c40f4e7743009724a1155bc
ani119 <karolina_makuch_email@wp.pl>
2020-05-04 17:25:29 +0200

    #42 Add clearing background
    - rename shoaldrag to shoal
    - redefine canvas hierarchy
    - add clearWater function

--- podwodny_swiat_tabliczki_mnozenia/Assets/Scritps/Multiplication.cs
+++ podwodny_swiat_tabliczki_mnozenia/Assets/Scritps/Multiplication.cs
@@ -37,6 +37,7 @@ public class Multiplication : MonoBehaviour
         if (answer.text == (multiplicand * multiplier).ToString())
         {
             error.text = "Bardzo dobrze! Następne równanie.";
+            GameController.clearWater();
             RandomEquation();
         }
         else
---
+++ podwodny_swiat_tabliczki_mnozenia/Assets/Scritps/Shoal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoal : MonoBehaviour
+{
+    public GameObject prefab;
+    public Vector3 newposition;
+    public GameObject obj;
+    int index = 0;
+    Vector3 mousePos;
+    
+    public void BeginDrag()
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = 2.0f;
+        newposition = Camera.main.ScreenToWorldPoint(mousePos);
+        obj = Instantiate(prefab, newposition, Quaternion.identity);
+        obj.name = this.name +"_"+ index.ToString();
+        obj.transform.parent = GameObject.Find("Canvas/Shoals/ShoalCopies").transform;
+        index++;
+    }
+
+    public void OnMouseDrag()
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = 2.0f;
+        newposition = Camera.main.ScreenToWorldPoint(mousePos);
+        obj.transform.position = newposition;
+    }
+}
